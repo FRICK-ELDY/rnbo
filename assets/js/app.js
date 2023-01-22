71129e81a9c2c06a5ef6e67bddb8da037259d0b0
tatmos <charytanaka@gmail.com>
2023-01-22 16:19:01 +0900

    add renbo files

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ import "../css/app.css"
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
+import "../rnbo/js/app"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
